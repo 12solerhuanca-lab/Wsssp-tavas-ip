69a3b782b80318912c674b4d83a8cbcd0eb78f42
12solerhuanca-lab <12solerhuanca@gmail.com>
2026-02-09 23:16:00 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+const VERIFY_TOKEN = "mitoken123";
+
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode === "subscribe" && token === VERIFY_TOKEN) {
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.post("/webhook", (req, res) => {
+  console.log("Mensaje recibido:", req.body);
+  res.sendStatus(200);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Servidor activo");
+});
